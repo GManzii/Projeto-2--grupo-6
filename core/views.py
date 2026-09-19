e4788b2b05a6b7e4fcd5206849724080385841bc
GManzii <gabrielamanzicorreia@gmail.com>
2026-09-19 17:12:12 -0300

    correcao contact

--- core/views.py
+++ core/views.py
@@ -5,10 +5,8 @@ from .forms import ContatoForm
 def home(request):
     return render(request, "core/home.html")
 
-
 def about(request):
     return render(request, "core/about.html")
-
 
 def contact(request):
     if request.method == 'POST':
@@ -16,8 +14,8 @@ def contact(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Sua mensagem foi enviada com sucesso!')
-            return redirect('fale_conosco')
+            return redirect('contact') 
     else:
         form = ContatoForm()
 
-    return render(request, 'contact.html', {'form': form})
+    return render(request, 'core/contact.html', {'form': form}) 
