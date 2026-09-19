a53506e257bda713819368bfd18a9f2c38cc7c7f
Isabela Melo da Silva <isamelodasilva20@gmail.com>
2026-09-19 16:32:43 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# formulario de contato
+class MensagemContato(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    mensagem = models.TextField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.email}"
+
 # 1. Perfil da Empresa
 class Empresa(models.Model):
     nome = models.CharField(max_length=100)
